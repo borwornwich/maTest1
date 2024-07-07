b5b7a46f42a8f9fe03f1d60bc31726ea9b1fc966
Borwornwich <famzuza79@gmail.com>
2024-07-07 15:50:56 +0900

    MaTest1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import myimg from './assets/Screenshot 2024-04-28 152858.png';
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return <>
+  <div>
+    <h1>Borwornwich</h1>
+    <img src={myimg} width="500" height="600"></img>
+  </div>
+  </>;
+  
+}
+
+export default App
